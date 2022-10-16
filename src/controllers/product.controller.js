08589288b98a81fdfca9c6a5ef0c28563490004e
Dan Abisai Flores Hernandez <danflores9975@gmail.com>
2022-10-16 00:35:01 -0500

    rec

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -12,6 +12,24 @@ export const getPS = async(req, res)=>{
     
 }
 
+export const getPC = async(req, res)=>{
+
+    try{
+        const [result] = await pool.query('SELECT * FROM product WHERE category = ?;',[req.query.cat]);
+    
+        if(result.length <= 0 ) return res.status(404).json({
+            message:'La busqueda no coincide con ningun producto.'
+        })
+        
+        res.json(result);
+    } catch (error) {
+            return res.status(500).json({
+            message: 'Algo salio mal!'
+        })
+    }
+
+}
+
 export const getP = async(req, res)=>{
 
     try{
@@ -30,20 +48,3 @@ export const getP = async(req, res)=>{
 
 }
 
-export const getPC = async(req, res)=>{
-
-    try{
-        const [result] = await pool.query('SELECT * FROM product WHERE category = ?;',[req.query.cat]);
-    
-        if(result.length <= 0 ) return res.status(404).json({
-            message:'La busqueda no coincide con ningun producto.'
-        })
-        
-        res.json(result);
-    } catch (error) {
-            return res.status(500).json({
-            message: 'Algo salio mal!'
-        })
-    }
-
-}
