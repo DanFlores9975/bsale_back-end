f080e11f58266e88c1c9979360b40099947fdc6b
Dan Abisai Flores Hernandez <danflores9975@gmail.com>
2022-10-16 00:19:00 -0500

    Nuevo endpoint añadido
    Se agrega endpoint que retorna solo los productos de una categoria

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -29,3 +29,21 @@ export const getP = async(req, res)=>{
     }
 
 }
+
+export const getPC = async(req, res)=>{
+
+    try{
+        const [result] = await pool.query('SELECT * FROM product WHERE category = ?;',[req.query.cat]);
+    
+        if(result.length <= 0 ) return res.status(404).json({
+            message:'La busqueda no coincide con ningun producto.'
+        })
+        
+        res.json(result);
+    } catch (error) {
+            return res.status(500).json({
+            message: 'Algo salio mal!'
+        })
+    }
+
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getP, getPS } from "../controllers/product.controller.js";
+import { getP, getPS, getPC } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.get("/", getPS);
 router.get("/search", getP);
+router.get("/category", getPC);
 
 
 export default router;
